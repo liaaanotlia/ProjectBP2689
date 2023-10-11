406ad440e13ca850a281fe3524596628ff54f529
liaaanotlia <effieamalia@gmail.com>
2023-10-11 17:02:10 +0700

    praktikum 6

---
+++ app/src/main/java/com/example/projectbp2689/BukuModel.kt
@@ -0,0 +1,4 @@
+package com.example.projectbp2689
+
+class BukuModel(var image:Int, var title:String, var desc:String) {
+}
--- app/src/main/java/com/example/projectbp2689/HomeActivity.kt
+++ app/src/main/java/com/example/projectbp2689/HomeActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +13,20 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
         //instance
         val btnBookKid:Button = findViewById(R.id.buttonMenu1)
-        //event btn book kid di-klik
+        val rvBuku:RecyclerView = findViewById(R.id.recyclerViewBuku)
+        //set layout manager di RecyclerView
+        rvBuku.layoutManager = LinearLayoutManager(this)
+        //list data buku
+        val data = ArrayList<BukuModel>()
+        data.add(BukuModel(R.drawable.book1,"Emi's Beach Adventure","Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet"))
+        data.add(BukuModel(R.drawable.book2,"Ade's Adventure","Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet"))
+        data.add(BukuModel(R.drawable.book4,"Mermaid To Rescue","Lorem Ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet"))
+        //set adapter
+        val adapter = AdapterHome(data)
+        //set adapter ke recycler view
+        rvBuku.adapter = adapter
 
+        //event btn book kid di-klik
         btnBookKid.setOnClickListener {
             val intentBookKid = Intent(this,BookKidActivity::class.java)
             startActivity(intentBookKid)
